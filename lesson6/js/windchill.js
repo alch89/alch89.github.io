ed32cfcb69742a72162c6fa6d19178a7686767fd
alch89 <alch89@byui.edu>
2021-02-14 02:14:56 -0500

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,9 @@
-function doInputOutput(){
-    let temperature = parseInt(document.getElementById('temp').value);
-    let windSpeed = parseInt(document.getElementById('wSpeed').value);
+
+    let temperature = parseFloat(document.getElementById('temp').innerHTML);
+    let windSpeed = parseFloat(document.getElementById('wSpeed').innerHTML);
     let result = windChill(temperature, windSpeed)
     document.getElementById('outputDiv').innerHTML = result.toFixed(0);
-}
+
 
 function windChill(tempF, speed){
     return 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16)
