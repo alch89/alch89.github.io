e867f4836d76e21faf285b36a089cd6e0f7c71a8
alch89 <alch89@byui.edu>
2021-03-21 00:06:36 -0500

    commit

--- lesson10/js/apicall.js
+++ lesson10/js/apicall.js
@@ -26,3 +26,40 @@ fetch(requestURLweather)
         let finalResult = (t <= 50 && s > 3) ? `${f}°F` : 'N/A';
         document.getElementById('windchill').textContent = finalResult;
     });
+
+
+
+    const apiURLForecast =
+  "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=65aaffae0d89798b595cc40e7e0e01d0&units=imperial";
+
+    fetch(apiURLForecast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const forecast = jsObject["list"].filter((day) =>
+      day.dt_txt.includes("18:00:00")
+    );
+
+    for (let i = 0; i < 5; i++) {
+      let day = forecast[i];
+      
+      let date = new Date(day.dt_txt);
+      let weekday = weekdays[date.getDay()];
+      document.querySelector(
+        `#fiveday thead>tr>th:nth-child(${i + 1})`
+      ).innerText = weekday;
+
+      let imagesrc = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+      let desc = day.weather[0].description;
+      document
+        .querySelector(`#fiveday tbody>tr>td:nth-child(${i + 1}) img`)
+        .setAttribute("src", imagesrc);
+      document
+        .querySelector(`#fiveday tbody>tr>td:nth-child(${i + 1}) img`)
+        .setAttribute("alt", desc);
+
+      document.querySelector(
+        `#fiveday tbody>tr>td:nth-child(${i + 1}) span`
+      ).innerText = `${Math.round(day.main.temp)}`;
+    }
+  });
